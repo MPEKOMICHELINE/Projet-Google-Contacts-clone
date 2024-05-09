064397b4171225d96d6429e9531670f9bddda632
micheline mpeko <mpekodiembimicheline@gmail.com>
2024-05-09 08:43:40 +0200

    je change la structure

---
+++ script.js
@@ -0,0 +1,5 @@
+// Sélectionnez l'élément html (la page entière)
+var page = document.documentElement;
+
+// Changez la couleur de fond
+page.style.backgroundColor = "#f0f8ff"; // Couleur bleu clair
