6f0606d0823a603988fb6988e23f445876b55b27
Micheline Mpeko <mpekodiembimicheline@gmail.com>
2024-05-10 12:17:02 +0100

    html et css

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ var page = document.documentElement;
 
 // Changez la couleur de fond
 page.style.backgroundColor = "#f0f8ff"; // Couleur bleu clair
+
+
+const btnShow = document.querySelector("complement__btn");
+const answer = document.querySelector("container__creer");
+const clickBtn = function(){
+    if (answer.classList.toggle("Créer un contact")) {
+        btnShow.textContent = "Créer un contact";
+      
+      }
+};
+btnShow.addEventListener("click", clickBtn);
