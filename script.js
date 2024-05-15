12fd6e044c6e188adcb9e8647bfa727ebac1a6f6
Micheline Mpeko <mpekodiembimicheline@gmail.com>
2024-05-15 12:18:53 +0100

    ajout de html et css

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
 const page = document.documentElement; // Sélectionnez l'élément html (la page entière)
 page.style.backgroundColor = "#f0f8ff"; // Changez la couleur de fond
 
-const clickBtn = document.querySelector("#button");
-const afficheMain = document.querySelector("#main");
+const clickBtn = document.querySelector("button");
+const afficheMain = document.querySelector("main");
+clickBtn = false
 clickBtn.addEventListener("click",() => {
-  for(let i= 0; i<afficheMain.clientHeight; i++){
-    console.log(afficheMain.innerHTML);
+  if (afficheMain){
+    afficheMain.style.display = "grid"
+    
   }
+ else{
+  afficheMain.style.display = "none";
+
+ }
+  
 }
 )
