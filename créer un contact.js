9373f4fc992ac309fd344b77176de35692bd56b0
Micheline Mpeko <mpekodiembimicheline@gmail.com>
2024-05-16 12:14:41 +0100

    js

---
+++ créer un contact.js
@@ -0,0 +1,8 @@
+//  const complementBtn = document.querySelector("#complement__btn");
+//  const containerCreer = document.querySelector("container__creer");
+// complementBtn.addEventListener('click', function(){
+    
+// }
+// )
+
+
--- script.js
+++ script.js
@@ -2,18 +2,38 @@
 const page = document.documentElement; // Sélectionnez l'élément html (la page entière)
 page.style.backgroundColor = "#f0f8ff"; // Changez la couleur de fond
 
-const clickBtn = document.querySelector("button");
-const afficheMain = document.querySelector("main");
-clickBtn = false
-clickBtn.addEventListener("click",() => {
-  if (afficheMain){
-    afficheMain.style.display = "grid"
-    
-  }
- else{
-  afficheMain.style.display = "none";
+const clickBtn = document.querySelector("#button");
 
- }
-  
+
+function visibiliteMenu() {
+  const containerComplement = document.querySelector('.container__complement')
+  if (containerComplement.style.display == 'none') {
+    containerComplement.style.display = 'block'
+  }
+  else{
+    containerComplement.style.display = 'none'
+  }
 }
-)
+
+clickBtn.addEventListener('click',visibiliteMenu)
+
+
+const complementBtn = document.querySelector('#complement__btn');
+
+
+
+function menuVisible () {
+  const Creer = document.querySelector('.creer')
+  if ( Creer.style.display =='none'){
+    Creer.style.display ='block'
+  }
+  else{
+    Creer.style.display ='none'
+  }
+}
+complementBtn.addEventListener('click',menuVisible)
+
+
+const Creer = document.querySelector('.creer');
+const containerCreer = document.querySelector('#containerCreer');
+containerContacts = document.querySelector('ontainer__contacts');
