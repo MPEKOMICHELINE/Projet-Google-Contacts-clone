227588740553ebbc11449b701a0bd92095c85d1b
micheline mpeko <mpekodiembimicheline@gmail.com>
2024-05-20 08:59:37 +0200

    je travais sur js

--- script.js
+++ script.js
@@ -1,39 +1,115 @@
 
-const page = document.documentElement; // Sélectionnez l'élément html (la page entière)
-page.style.backgroundColor = "#f0f8ff"; // Changez la couleur de fond
 
-const clickBtn = document.querySelector("#button");
+const clickBtn = document.querySelector('#button');
+const containerComplement = document.querySelector('.container__complement')
+const complementBtn = document.querySelector('.complement__btn');
+const complementBtn2 = document.querySelector('.complement__btn2');
+const Creer = document.querySelector('.creer')
+const containerCreer = document.querySelector('.container__creer');
+const containerContacts = document.querySelector('.container__contacts');
+const containerForm = document.querySelector('.containerCreer___contacts');
+const enregistrerImg = document.querySelector('#enregistrer__img');
+const libelleBtn = document.querySelector('.libelle__btn ');
+const keyboardKey = document.querySelector('.keyboard__key');
+const Keyboard = document.querySelector('.keyboard__key');
 
 
 function visibiliteMenu() {
-  const containerComplement = document.querySelector('.container__complement')
+  document.querySelector('.container__complement')
   if (containerComplement.style.display == 'none') {
     containerComplement.style.display = 'block'
+    containerCreer.classList.remove("container__complement__fill")
   }
   else{
-    containerComplement.style.display = 'none'
-  }
+    containerComplement.style.display = 'none'  
+    containerCreer.classList.add("container__complement__fill")
 }
 
+}
+
+function visibiliteContactsFormulaire(){
+  
+    containerContacts.style.display="none";
+    containerForm.style.display= 'block';
+
+}
+
+function hideContactForm(){
+  containerContacts.style.display="block";
+    containerForm.style.display= 'none';
+}
+
+function buttonFleche(){
+  containerForm.style.display= 'none';
+  containerContacts.style.display='block';
+}
+
+
+function visibiliteKeyboard(){
+  if( keyboardKey.style.display='none'){
+    keyboardKey.style.display='block';
+  }
+  else{
+    keyboardKey.style.display='none';
+
+}
+
+}
+
+
+
+Keyboard.addEventListener('click',visibiliteKeyboard)
+enregistrerImg.addEventListener('click',buttonFleche)
+complementBtn2.addEventListener('click',hideContactForm)
+complementBtn.addEventListener('click',visibiliteContactsFormulaire)
 clickBtn.addEventListener('click',visibiliteMenu)
 
 
-const complementBtn = document.querySelector('#complement__btn');
 
 
 
-function menuVisible () {
-  const Creer = document.querySelector('.creer')
-  if ( Creer.style.display =='none'){
-    Creer.style.display ='block'
-  }
-  else{
-    Creer.style.display ='none'
-  }
-}
-complementBtn.addEventListener('click',menuVisible)
 
 
-const Creer = document.querySelector('.creer');
-const containerCreer = document.querySelector('#containerCreer');
-containerContacts = document.querySelector('ontainer__contacts');
+
+
+
+
+
+
+
+
+/*let contacts = [
+  {
+    id: 1, 
+    name: 'David',
+    email: 'david@gmail.com', 
+    phone: '22322323', 
+    fonction: 'kadea.academy', 
+    labelles: ['label, label2, label3'],
+  },
+  {
+    id: 1, 
+    name: 'Richard',
+    email: 'david@gmail.com', 
+    phone: '22322542', 
+    fonction: 'kadea.academy', 
+    labelles: ['label, label2, label3'],
+  },
+  {
+    id: 1, 
+    name: 'Hello',
+    email: 'david@gmail.com', 
+    phone: '26622323', 
+    fonction: 'kadea.academy', 
+    labelles: ['label, label2, label3'],
+  },
+];
+
+function renderContacts () {
+  let tableBody = document.querySelector('.tableBody');
+  let tableRowsHTML = '';
+  
+}*/
+
+
+
